293684b11849f2fb495982f5ac4242353e4dc300
tbezerrasoares <tbezerrasoares@gmail.com>
2026-09-15 09:28:44 -0300

    Add Servidor.Java

--- Techlab/Servidor.java
+++ Techlab/Servidor.java
@@ -1,5 +1,21 @@
 package Techlab;
 
-public class Servidor {
+public class Servidor extends AtivoTI{
+    private String ipEstatico;
+    private String sistemaOperacional;
+    private int memoriaRam;
+
+    public Servidor(int id, String patrimonio, String modelo, String status, String ipEstatico
+        , String sistemaOperacional, int memoriaRam){
+                super(id, patrimonio, modelo, status);
+                this.ipEstatico = ipEstatico;
+                this.sistemaOperacional = sistemaOperacional;
+                this.memoriaRam = memoriaRam;
+            }
     
+    @Override
+    public String toString(){
+        return "[Servidor] " +super.toString()+ " | IP: " +ipEstatico+ " | SO: " +sistemaOperacional;
+    }
+
 }
